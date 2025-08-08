7acdab06b19a5c5f304f563f03aa2f0239c0bd24
PSP <Pradeeppulla72@gmail.com>
2025-08-08 11:28:52 +0530

    Added Frontend.sh script

--- Frontend.sh
+++ Frontend.sh
@@ -1,10 +1,10 @@
 dnf module list nginx
 
-cp nginx.conf /etc/nginx/nginx.conf
-
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
+
+cp nginx.conf /etc/nginx/nginx.conf
 
 systemctl enable nginx
 systemctl start nginx

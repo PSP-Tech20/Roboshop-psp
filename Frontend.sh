0f6bc189c17c8fb80120c6a144fd9a330c320ac9
PSP <Pradeeppulla72@gmail.com>
2025-08-08 18:00:30 +0530

    Code dry service_setup, Systemctl_setup, conf_setup, repo_setup

--- Frontend.sh
+++ Frontend.sh
@@ -7,7 +7,7 @@ dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 
-conf_setup
+cp nginx.conf /etc/nginx/nginx.conf
 
 systemctl enable nginx
 systemctl start nginx
--- common.sh
+++ common.sh
@@ -8,9 +8,6 @@ service_setup() {
   cp $(component).service /etc/systemd/system/$(component).service
 }
 
-conf_setup() {
-  cp $(component).conf /etc/$(component)/$(component).conf
-}
 
 repo_setup() {
   cp $(component).repo /etc/yum.repos.d/$(component).repo
